bf87700cdd9b316c1ae7e9bbaa0d312395b5f11f
HorusAlkebulan <hmajr@outlook.com>
2015-12-17 08:40:36 -0800

    My first commit

---
+++ GitHubMVC/GitHubMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubMVC.Startup))]
+namespace GitHubMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
